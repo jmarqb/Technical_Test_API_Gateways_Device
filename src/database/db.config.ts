87ccd70923cbd0f8ed4daec9b253884a4f683ce8
jmarqb <jmarqbvpn@gmail.com>
2023-08-06 22:09:10 -0500

    Changes made in the database configuration file

--- src/database/db.config.ts
+++ src/database/db.config.ts
@@ -1,16 +1,26 @@
-import { createPool } from "mysql2/promise";
+import { createPool, Pool } from "mysql2/promise";
 import 'dotenv/config';
 
-
+let connection: Pool | null = null;
 export const connect =async()=>{
 
-  const connection = createPool({
-        host:     process.env.DB_HOST,
-        user:     process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        port:     Number(process.env.DB_PORT),
-        connectionLimit: 10
-    });
+  if(!connection){
+    connection = createPool({
+          host:     process.env.DB_HOST,
+          user:     process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+          port:     Number(process.env.DB_PORT),
+          connectionLimit: 10,
+          timezone: 'Z'
+      });
+  }
     return connection;
+}
+
+export const disconnect = async () => {
+  if (connection) {
+    const p = await connection;
+    await p.end();
+  }
 }
